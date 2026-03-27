03ca33e4f7d36281236e47e7243935f9991d6812
Anton Wessel <anton.wessel01@outlook.com>
2026-03-27 22:24:02 +0100

    finish net income thing, ask for expenses too

--- calculator.py
+++ calculator.py
@@ -6,4 +6,11 @@ print("Milk chocolate: $1680")
 print("Doughnut: $1075")
 print("Pancake: $80")
 
-print("Income: $" + str(202 + 118 + 2250 + 1680 + 1075 + 80))
+income = 202 + 118 + 2250 + 1680 + 1075 + 80
+print(f"Income: ${income}")
+
+print("Staff expenses:")
+staff_expenses = int(input())
+print("Other expenses:")
+other_expenses = int(input())
+print(f"Net income: ${income - staff_expenses - other_expenses}")
